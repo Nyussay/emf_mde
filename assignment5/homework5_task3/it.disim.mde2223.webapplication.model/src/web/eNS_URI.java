17011847c3efad1777b8df84e44f7a26a09b03d1
Nyussay <58169429+Nyussay@users.noreply.github.com>
2023-01-11 11:14:33 +0100

    Add files via upload

---
+++ assignment5/homework5_task3/it.disim.mde2223.webapplication.model/src/web/eNS_URI.java
@@ -0,0 +1,5 @@
+package web;
+
+public interface eNS_URI {
+
+}
---
+++ assignment5/homework5_task3/it.disim.mde2223.webapplication.model/src/web/impl/webapplicationImpl.java
@@ -0,0 +1,187 @@
+/**
+ */
+package web.impl;
+
+import java.util.Collection;
+
+import org.eclipse.emf.common.notify.NotificationChain;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.eclipse.emf.ecore.util.EObjectContainmentEList;
+import org.eclipse.emf.ecore.util.InternalEList;
+
+import web.Entity;
+import web.Index;
+import web.WebPackage;
+import web.webapplication;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>webapplication</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * </p>
+ * <ul>
+ *   <li>{@link web.impl.webapplicationImpl#getEntities <em>Entities</em>}</li>
+ *   <li>{@link web.impl.webapplicationImpl#getIndexes <em>Indexes</em>}</li>
+ * </ul>
+ *
+ * @generated
+ */
+public class webapplicationImpl extends NamedElementImpl implements webapplication {
+	/**
+	 * The cached value of the '{@link #getEntities() <em>Entities</em>}' containment reference list.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getEntities()
+	 * @generated
+	 * @ordered
+	 */
+	protected EList<Entity> entities;
+
+	/**
+	 * The cached value of the '{@link #getIndexes() <em>Indexes</em>}' containment reference list.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getIndexes()
+	 * @generated
+	 * @ordered
+	 */
+	protected EList<Index> indexes;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected webapplicationImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return WebPackage.Literals.WEBAPPLICATION;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EList<Entity> getEntities() {
+		if (entities == null) {
+			entities = new EObjectContainmentEList<Entity>(Entity.class, this, WebPackage.WEBAPPLICATION__ENTITIES);
+		}
+		return entities;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EList<Index> getIndexes() {
+		if (indexes == null) {
+			indexes = new EObjectContainmentEList<Index>(Index.class, this, WebPackage.WEBAPPLICATION__INDEXES);
+		}
+		return indexes;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
+		switch (featureID) {
+			case WebPackage.WEBAPPLICATION__ENTITIES:
+				return ((InternalEList<?>)getEntities()).basicRemove(otherEnd, msgs);
+			case WebPackage.WEBAPPLICATION__INDEXES:
+				return ((InternalEList<?>)getIndexes()).basicRemove(otherEnd, msgs);
+		}
+		return super.eInverseRemove(otherEnd, featureID, msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case WebPackage.WEBAPPLICATION__ENTITIES:
+				return getEntities();
+			case WebPackage.WEBAPPLICATION__INDEXES:
+				return getIndexes();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case WebPackage.WEBAPPLICATION__ENTITIES:
+				getEntities().clear();
+				getEntities().addAll((Collection<? extends Entity>)newValue);
+				return;
+			case WebPackage.WEBAPPLICATION__INDEXES:
+				getIndexes().clear();
+				getIndexes().addAll((Collection<? extends Index>)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case WebPackage.WEBAPPLICATION__ENTITIES:
+				getEntities().clear();
+				return;
+			case WebPackage.WEBAPPLICATION__INDEXES:
+				getIndexes().clear();
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case WebPackage.WEBAPPLICATION__ENTITIES:
+				return entities != null && !entities.isEmpty();
+			case WebPackage.WEBAPPLICATION__INDEXES:
+				return indexes != null && !indexes.isEmpty();
+		}
+		return super.eIsSet(featureID);
+	}
+
+} //webapplicationImpl
